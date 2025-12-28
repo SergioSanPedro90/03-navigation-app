71c1ef38d45582f89830c331798c7292b0904e14
Sergio <sergiospalvarez@hotmail.com>
2025-12-28 23:33:37 +0100

    app con navegacion

---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from "expo-router";
+import "./global.css"
+import { useEffect } from 'react';
+
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+
+  const [ fontsLoaded, error ] = useFonts({
+    'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
+    'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
+    'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf')
+  })
+
+  useEffect(()=> {
+    if (error) throw error;
+
+    if (fontsLoaded) SplashScreen.hideAsync();
+    
+
+  }, [fontsLoaded, error])
+
+  if (!fontsLoaded && !error) return null;
+
+  return <Stack />;
+}
---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+
+const App = () => {
+  return (
+    
+      <View className="flex-1 justify-center items-center bg-red-500">
+        <Text className="text-2xl font-work-black">index</Text>
+        <Text className="text-2xl font-work-light">index</Text>
+        <Text className="text-2xl font-work-medium">index</Text>
+      </View>
+    
+  );
+};
+export default App;
