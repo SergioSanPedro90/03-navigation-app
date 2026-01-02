620c9cb96c1e19cd8295e0295706132fca530517
Sergio <sergiospalvarez@hotmail.com>
2026-01-02 11:11:18 +0100

    guardado

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,8 @@
-import { Link, Redirect, router } from "expo-router";
-import { View, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import BotonsComponent from "../components/boton/botons";
+import { Redirect} from "expo-router";
 
 const App = () => {
 
-  return <Redirect href={'/(stacks)/home'}/>
+  return <Redirect href={"/tabs" as any}/>
 
   // return (
   //   <SafeAreaView>
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,24 @@
+import { Tabs } from 'expo-router'
+import { Ionicons } from '@expo/vector-icons'
+
+const TabsLayout = () => {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'green' }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: 'Inicio',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="home-outline" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: 'Favoritos',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="star-outline" color={color} />,
+        }}
+      />
+    </Tabs>
+  )
+}
+export default TabsLayout
